fa53c5018c664808987f7f0e4fbcffb3aa5cd6b4
afterwind-io <spmushroom@hotmail.com>
2023-07-11 11:06:07 +0800

    :recycle: 调整测试代码

---
+++ README.md
@@ -0,0 +1,11 @@
+# rust-json
+
+## Reference
+
+http://www.ecma-international.org/publications/files/ECMA-ST/ECMA-404.pdf
+
+https://tools.ietf.org/html/rfc8259
+
+http://seriot.ch/parsing_json.php
+
+https://github.com/nst/JSONTestSuite
--- src/test.rs
+++ src/test.rs
@@ -12,28 +12,31 @@ pub fn run_suite() {
         .map(|res| res.path())
         .collect();
 
+    let total = entries.len();
+    let mut index = 1;
+
     for entry in entries {
         let filename = entry.file_name().unwrap();
+        println!("({}/{}) {:?}", index, total, filename);
 
         match read_file_as_utf8(&entry) {
-            Err(x) => {
-                println!("{:?} {:?}\n", filename, x);
-                continue;
+            Err(reason) => {
+                println!("{:?}", reason);
+                println!("------------------------");
             }
             Ok(document) => {
                 let reader = UTF8Reader::new(&document);
+                let result = validate(&reader);
                 let expect = &filename.to_str().unwrap()[0..1];
 
-                let result = validate(&reader);
                 println!(
-                    "[{}] {:?}\n",
+                    "[{}]\n",
                     match result {
                         Ok(_) if expect == "y" => "Pass".bright_green(),
                         Err(_) if expect == "n" => "Pass".bright_green(),
                         _ if expect == "i" => "Pass".bright_green(),
                         _ => "Fail".bright_red(),
                     },
-                    filename
                 );
                 println!(
                     "{}\n\n{}\n------------------------",
@@ -45,5 +48,7 @@ pub fn run_suite() {
                 );
             }
         }
+
+        index += 1;
     }
 }
